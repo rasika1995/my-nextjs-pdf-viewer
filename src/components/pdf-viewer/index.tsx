a45bec8e37af2fc02ad8bfd4ca6f95b74374da87
Rasika Priyadarshani <rasikap@alliontechnologies.com>
2024-09-26 22:33:17 +0530

    Update PDFViewer to use <mark> to highlights text

--- src/components/pdf-viewer/index.tsx
+++ src/components/pdf-viewer/index.tsx
@@ -1,18 +1,8 @@
 'use client';
-import { TextItem } from 'pdfjs-dist/types/src/display/api';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
-
-interface Field {
-  text: string;
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-  pageHeight: number;
-}
 
 interface PDFViewerProps {
   pdfUrl: string;
@@ -21,7 +11,6 @@ interface PDFViewerProps {
 
 const PDFViewer: React.FC<PDFViewerProps> = ({ pdfUrl, textsToHighlight }) => {
   const [numPages, setNumPages] = useState<number | null>(null);
-  const [fieldsToHighlight, setFieldsToHighlight] = useState<Field[][]>([]);
 
   // Ensure PDF.js worker is set
   pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
@@ -30,45 +19,10 @@ const PDFViewer: React.FC<PDFViewerProps> = ({ pdfUrl, textsToHighlight }) => {
     setNumPages(pdf.numPages);
   };
 
-  useEffect(() => {
-    const fetchPDF = async () => {
-      try {
-        const pdf = await pdfjs.getDocument(pdfUrl).promise;
-
-        const fields = await Promise.all(
-          Array.from({ length: pdf.numPages }, async (_, i) => {
-            const page = await pdf.getPage(i + 1);
-            const viewport = page.getViewport({ scale: 1 });
-            const textContent = await page.getTextContent();
-
-            return textContent.items
-              .filter((item): item is TextItem => 'str' in item) // Type guard to filter only TextItems
-              .filter((item) => textsToHighlight.some(text => item.str.includes(text)))
-              .map((item) => ({
-                text: item.str,
-                x: item.transform[4],
-                y: item.transform[5],
-                width: item.width,
-                height: item.height,
-                pageHeight: viewport.height,
-              }));
-          })
-        );
-
-        setFieldsToHighlight(fields);
-      } catch (error) {
-        console.error('Error fetching PDF:', error);
-      }
-    };
-
-    fetchPDF();
-  }, [pdfUrl, textsToHighlight]);
-
   // Custom text renderer to highlight specified texts
   const customTextRenderer = ({ str }: { str: string; itemIndex: number }) => {
     // Check if the current text should be highlighted
-    const isHighlighted = fieldsToHighlight.flat().some(f => f.text === str);
-    
+    const isHighlighted = textsToHighlight.some(text => text === str);
     return isHighlighted ? `<mark>${str}</mark>` : str;
   };
 
