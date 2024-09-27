d67160b465d12a8ab83d64c591a07ba42a9c322c
Rasika Priyadarshani <rasikap@alliontechnologies.com>
2024-09-27 18:39:39 +0530

    update pdf-viewer to support multi-page pdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,21 @@ const MyApp = dynamic(() => import("@/components/pdf-viewer"), { ssr: false });
 // const ReactPDF2 = dynamic(() => import("@/components/react-pdf-2"), { ssr: false });
 
 const HomePage: NextPage = () => {
+  // const textToHighlights = [
+  //   "063010",
+  //   "231 Swanston St, Melbourne, VIC 3000, Australia",
+  //   "30 days after invoice date",
+  // ];
   const textToHighlights = [
-    "063010",
-    "231 Swanston St, Melbourne, VIC 3000, Australia",
-    "30 days after invoice date",
+    "May 2001",
+    "Primary bookmarks in a PDF file.",
+    "This sample package contains:",
+    "Window > Show Bookmarks"
   ];
   return (
     // <MyApp pdfUrl='/index.pdf'/>
-    <MyApp pdfUrl="/index.pdf" textsToHighlight={textToHighlights} />
+    <MyApp pdfUrl="/multiPage.pdf" textsToHighlight={textToHighlights} />
+    // <MyApp pdfUrl="/index.pdf" textsToHighlight={textToHighlights} />
     // <ReactPDF2 pdfUrl="/index.pdf" />
     // <MyDocument />
   );
--- src/components/pdf-viewer/index.tsx
+++ src/components/pdf-viewer/index.tsx
@@ -1,18 +1,20 @@
-'use client';
-import React, { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
+"use client";
+import React, { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
 
 interface PDFViewerProps {
   pdfUrl: string;
   textsToHighlight: string[];
 }
 
+
 const PDFViewer: React.FC<PDFViewerProps> = ({ pdfUrl, textsToHighlight }) => {
   const [numPages, setNumPages] = useState<number | null>(null);
 
   // Ensure PDF.js worker is set
+  // https://mozilla.github.io/pdf.js/
   pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
   const onDocumentLoadSuccess = (pdf: pdfjs.PDFDocumentProxy) => {
@@ -21,19 +23,20 @@ const PDFViewer: React.FC<PDFViewerProps> = ({ pdfUrl, textsToHighlight }) => {
 
   // Custom text renderer to highlight specified texts
   const customTextRenderer = ({ str }: { str: string; itemIndex: number }) => {
-    // Check if the current text should be highlighted
-    const isHighlighted = textsToHighlight.some(text => text === str);
-    return isHighlighted ? `<mark>${str}</mark>` : str;
+    const isHighlighted = textsToHighlight.some(
+      (text) => text.trim().toLowerCase() === str.trim().toLowerCase()
+    );
+    return isHighlighted ? `<mark>${str}</mark>` : str; // Return as string
   };
 
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{ width: "100vw", height: "100vh" }}>
       <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
         {Array.from(new Array(numPages), (el, index) => (
-          <Page 
-            key={`page_${index + 1}`} 
-            pageNumber={index + 1} 
-            customTextRenderer={customTextRenderer} 
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+            customTextRenderer={customTextRenderer}
           />
         ))}
       </Document>
